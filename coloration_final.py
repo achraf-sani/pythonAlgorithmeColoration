d0d05e052618fe0995db204d05d33b7825921ec3
achraf-sani <achraf.sani@esi.ac.ma>
2021-12-26 20:00:05 +0100

    v1.0.1 added comments

--- coloration_final.py
+++ coloration_final.py
@@ -1,13 +1,20 @@
 import numpy as np
 
 def coloration(mAdjacence):
+    """
+        main function
+    """
     degSommets = {}
+    #calcul des degrés des sommets
     for i in range(len(mAdjacence)):
         degSommets[i] = sum(mAdjacence[i])
     degSommetsTrié = sorted(degSommets.items(), key=lambda x: x[1], reverse=True)
-    couleurs = [0 for i in range(len(degSommetsTrié))]
 
+    #Initialisation de la liste des couleurs
+    couleurs = [0 for i in range(len(degSommetsTrié))]
     nbr_couleur_actuelle = 1
+
+    #Attribution des couleurs pour chaque sommet
     while sum([bool(c) for c in couleurs]) != len(degSommetsTrié):
         couleur_actuelle = "C" + str(nbr_couleur_actuelle)
         sommets_coloriés = []
@@ -19,7 +26,12 @@ def coloration(mAdjacence):
     
     return degSommetsTrié, couleurs
 
+
 def nonAdjacent(sommets_color, sommet, mAdjacence):
+    """
+        Fonction qui retourne True si un sommet n'est pas adjacent à une liste de sommets,
+        False sinon. 
+    """
     estNonAdjacent = True
     for s in sommets_color:
         if mAdjacence[sommet][s] == 1:
@@ -27,6 +39,10 @@ def nonAdjacent(sommets_color, sommet, mAdjacence):
     return estNonAdjacent
 
 def uneRéalisation(coloration, sommets):
+    """
+        Fonction qui retourne les sommets et leur couleur
+        sous forme de tableau.
+    """
     degSommet, couleurs = coloration[0], coloration[1]
     print("Sommet" + " "*6 + "deg(sommet)" + " "*6 +"couleur")
     for i in range(len(couleurs)):
@@ -36,17 +52,7 @@ def uneRéalisation(coloration, sommets):
         print(sommet.center(6), deg_sommet.rjust(11), couleur.rjust(14))
 
 
-
-
-
-
-exemple2 = np.array([[0, 1, 1, 0, 0, 0, 1], 
-                       [1, 0, 1, 1, 0, 0, 0], 
-                       [1, 1, 0, 0, 1, 1, 1], 
-                       [0, 1, 0, 0, 0, 0, 0],
-                       [0, 0, 1, 0, 0, 1, 0],
-                       [0, 0, 1, 0, 1, 0, 1],
-                       [1, 0, 1, 0, 0, 1, 0]])
+#exemple:
 
 sommets_exemple1 = ['A', 'B', 'C', 'D', 'E', 'F', 'G']
 exemple1 = np.array([[0, 1, 1, 0, 0, 0, 0], 
@@ -57,6 +63,5 @@ exemple1 = np.array([[0, 1, 1, 0, 0, 0, 0],
                        [0, 0, 0, 0, 1, 0, 0],
                        [0, 0, 0, 0, 1, 0, 0]])
 
-#print(coloration(exemple1))
-uneRéalisation(coloration(exemple1), sommets_exemple1)
 
+uneRéalisation(coloration(exemple1), sommets_exemple1)
